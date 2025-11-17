f00ea6cc7209b0fd647ee4ab7234ac51744dd382
soyoon <152174972+soyxni@users.noreply.github.com>
2025-11-17 16:06:15 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.16 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,23 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        Set<Integer> sumSet = new TreeSet<>();
+        
+        // 최대 200, 0~200 -> 201개
+        boolean[] check = new boolean[201];
         
         for(int i=0; i<numbers.length; i++){
             for(int j=i+1; j<numbers.length; j++){
-                sumSet.add(numbers[i]+numbers[j]);
+                check[numbers[i]+numbers[j]] = true;
             }
         }
         
-        // 변환
-        int[] answer = new int[sumSet.size()];
+        // true 개수 크기만큼 answer배열 생성
+        int count = 0;
         
+        for(int i=0; i<check.length; i++){
+            if(check[i] == true){
+                count++;
+            }
+        }
+        
+        int[] answer = new int[count];
+        
+        // answer에 값 저장 -> 체크의 인덱스를 저장해줌
         int index = 0;
-    
-        // 순차적으로 저장
-        for(int sum : sumSet){
-            answer[index] = sum;
-            index++;
+        for(int i=0; i<check.length; i++){
+            if(check[i]==true){
+                answer[index] = i;
+                index++;
+            }
         }
         
         return answer;
