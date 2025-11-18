f92b294f0e2097247e9596897a3793cc03229647
soyoon <152174972+soyxni@users.noreply.github.com>
2025-11-18 22:12:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 32.21 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,26 +1,14 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "0";
         
-        // 짝수개이면 그만큼 양옆에 / 홀수개이면 나머지 버리고
-        for(int i=1; i<food.length; i++){
-            int cnt = food[i]/2;
-            for(int j=0; j<cnt; j++){
-                sb.append(Integer.toString(i));
+        for(int i=food.length-1; i>0; i--){
+            // 끝 인덱스부터 시작
+            // 해당 인덱스 가능 갯수만큼 반복
+            for(int j=0; j<food[i]/2; j++){
+                answer = i + answer + i;
             }
         }
-        
-        StringBuilder sbInverse = new StringBuilder();
-        String halfStr = sb.toString();
-        // 역순 스트링
-        for(int i=0; i<halfStr.length(); i++){
-            sbInverse.append(halfStr.charAt(halfStr.length()-i-1));
-        }
-        
-        // 0(물) 붙이고 역순으로 붙이기
-        sb.append("0");
-        
-        String answer = sb.toString()+sbInverse.toString();
         return answer;
     }
 }
