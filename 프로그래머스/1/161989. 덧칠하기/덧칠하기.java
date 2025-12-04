4852eb52ce2fd3173c82fab180e084ccf2145e3d
soyoon <152174972+soyxni@users.noreply.github.com>
2025-12-04 11:01:30 +0900

    [level 1] Title: 덧칠하기, Time: 1.75 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        // 현재 롤러가 닿는 끝 지점
+        int end = 0;
+        
+        for(int i=0; i<section.length; i++){
+            if(end<section[i]){
+                end = section[i] + m -1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
